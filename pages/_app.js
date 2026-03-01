79aec2e38ff627b0e81596ade937fc92994efa0a
alghozii_zyy <91893301+alghoziii@users.noreply.github.com>
2026-03-02 01:15:00 +0700

    add layout enhancements and styling updates for improved UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
+import StarsBackground from "../components/background/StarsBackground";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="relative min-h-screen bg-slate-950">
+      <StarsBackground />
+      <div className="relative z-10">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
